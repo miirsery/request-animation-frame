44a5bb4f7fa550ea1f7fd8ab36b5d1ec2ce7428c
miirsery <miirsery.main@gmail.com>
2023-11-03 01:21:35 +0700

    feat(): add description for fps check

--- src/components/FpsCheck/FpsCheck.vue
+++ src/components/FpsCheck/FpsCheck.vue
@@ -3,55 +3,57 @@
     <button @click="increaseCounter">Click #{{ counter }}</button>
       {{ time.toFixed(3) }}s
     <br />
-	  
+
 	  <div class="fps-check__text">
 		  {{ fps !== null ? Math.floor(fps) + ' FPS' : 'Calculating FPS...' }}
 	  </div>
+
+    <h3>Используется для отображения FPS, которое выдает монитор</h3>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted, onBeforeUnmount } from "vue";
+import { ref, onMounted, onBeforeUnmount } from "vue"
 
-const time = ref(0);
-const fps = ref(null);
-const counter = ref(1);
+const time = ref(0)
+const fps = ref(null)
+const counter = ref(1)
 
-let init = performance.now();
-let last = init;
-let animationFrame = null;
-let initialized = false;
+let init = performance.now()
+let last = ref(init)
+let animationFrame = ref()
+let initialized = ref(false)
 
 const increaseCounter = () => {
-  counter.value++;
-};
+  counter.value++
+}
 
 const animate = () => {
-  const now = performance.now();
-  const deltaTime = (now - last) / 1000;
-  
-  if (!initialized) {
+  const now = performance.now()
+  const deltaTime = (now - last.value) / 1000
+
+  if (!initialized.value) {
     // Если это первая итерация, инициализируем fps
-    fps.value = 1 / deltaTime;
-    initialized = true;
+    fps.value = 1 / deltaTime
+    initialized.value = true
   } else {
-    fps.value = 1 / deltaTime;
+    fps.value = 1 / deltaTime
     if (counter.value % 2 === 0) {
-      time.value += deltaTime;
+      time.value += deltaTime
     }
   }
-  
-  last = now;
-  animationFrame = requestAnimationFrame(animate);
-};
+
+  last.value = now
+  animationFrame.value = requestAnimationFrame(animate)
+}
 
 onMounted(() => {
-  animationFrame = requestAnimationFrame(animate);
-});
+  animationFrame.value = requestAnimationFrame(animate)
+})
 
 onBeforeUnmount(() => {
-  cancelAnimationFrame(animationFrame);
-});
+  cancelAnimationFrame(animationFrame.value)
+})
 </script>
 
 <style lang="scss" scoped src="./styles.scss" />
